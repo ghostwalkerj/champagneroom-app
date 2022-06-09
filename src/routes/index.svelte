823b165d48ca1ca709a8c1635c03c16d393704c2
Crypto Joe <crypto.joe@sovereign.science>
2022-06-10 06:37:04 +0800

    form validation with yup

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,22 @@
 <script type="ts">
 	import ConnectButton from 'components/web3/ConnectButton.svelte';
 	import { selectedAccount } from 'svelte-web3';
+	import { createForm } from 'felte';
+	import { reporter, ValidationMessage } from '@felte/reporter-svelte';
+	import { validator } from '@felte/validator-yup';
+	import * as yup from 'yup';
+
+	const schema = yup.object({
+		name: yup.string().min(3).max(20).required(),
+		amount: yup.number().integer().min(0).max(10000).required()
+	});
+
+	const { form } = createForm<yup.InferType<typeof schema>>({
+		extend: [reporter, validator({ schema })],
+		onSubmit(values) {
+			console.log(values);
+		}
+	});
 </script>
 
 <div class="hero min-h-screen bg-base-200">
@@ -13,7 +29,45 @@
 			</p>
 
 			{#if $selectedAccount}
-				<button class="btn btn-primary">Get Started</button>
+				<form use:form>
+					<div class="p-2 flex flex-col text-left">
+						<div class="form-control w-full max-w-xs">
+							<label class="label">
+								<span class="label-text">What is your name?</span>
+							</label>
+							<input
+								type="text"
+								id="name"
+								name="name"
+								placeholder="Enter a name"
+								class="input input-bordered input-primary w-full max-w-xs"
+							/>
+							<ValidationMessage for="name" let:messages={message}>
+								<span>{message}</span>
+								<span slot="placeholder" />
+							</ValidationMessage>
+						</div>
+						<div class="form-control w-full max-w-xs">
+							<label class="label">
+								<span class="label-text">Requested Amount in USD</span>
+							</label>
+							<input
+								type="number"
+								id="amount"
+								name="amount"
+								placeholder="Enter an amount"
+								class="input input-bordered input-primary w-full max-w-xs"
+							/>
+							<ValidationMessage for="amount" let:messages={message}>
+								<span>{message}</span>
+								<span slot="placeholder" />
+							</ValidationMessage>
+						</div>
+						<div class="py-4">
+							<button class="btn btn-primary" type="submit">Generate Link</button>
+						</div>
+					</div>
+				</form>
 			{:else}
 				<ConnectButton />
 			{/if}
