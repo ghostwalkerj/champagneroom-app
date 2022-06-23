7bdbfe18877ebf6065e006e436e0f7b652fda240
Crypto Joe <crypto.joe@sovereign.science>
2022-06-23 17:15:22 +0800

    Fixed Jazzicon changing with new address

--- app/src/components/web3/JazzIcon.svelte
+++ app/src/components/web3/JazzIcon.svelte
@@ -1,24 +1,22 @@
 <script lang="ts">
-	import jazzicon from 'jazzicon-ts';
 	import { onMount } from 'svelte';
 	export let address: string;
 	let diameter = 20;
-	let container: HTMLDivElement;
 	$: hash = parseInt(address.slice(2, 10), 16);
-	let jazziconImport: { default: (arg0: number, arg1: number) => any };
-	let jazzicon: HTMLDivElement;
-
+	let container = document.getElementById('#jazzicon');
+	let jazzicon: { default: (arg0: number, arg1: number) => any };
+	let identicon: any;
 	onMount(async () => {
-		jazziconImport = await import('@metamask/jazzicon');
-		jazzicon = jazziconImport.default(diameter, hash);
-		container.appendChild(jazzicon);
+		jazzicon = await import('@metamask/jazzicon');
+		identicon = jazzicon.default(diameter, hash);
+		container.appendChild(identicon);
 	});
 
-	if (jazziconImport) {
-		jazzicon = jazziconImport.default(diameter, hash);
-		console.log(jazzicon);
+	$: if (jazzicon) {
+		const newIdenticon = jazzicon.default(diameter, hash);
+		container.replaceChild(newIdenticon, identicon);
+		identicon = newIdenticon;
 	}
 </script>
 
-{jazzicon}
-<div bind:this={container} />
+<div bind:this={container} id="#jazzicon" />
