0eb5f75563142ceef492ff10d7b841894993603f
jiyosub <crypto.joe@sovereign.science>
2022-07-03 11:57:34 +0800

    refactor Link object

--- app/src/db/models/link.ts
+++ app/src/db/models/link.ts
@@ -2,21 +2,48 @@ import urlJoin from 'url-join';
 import validator from 'validator';
 import { z } from 'zod';
 import { DocumentBase } from '.';
+
+export enum LinkStatus {
+	ACTIVE = 'ACTIVE',
+	EXPIRED = 'EXPIRED',
+	IN_PROGRESS = 'IN_PROGRESS',
+	COMPLETED = 'COMPLETED'
+}
 export const linkSchema = z.object({
-	name: z
-		.string()
-		.min(3, { message: 'Must be 3 or more characters long' })
-		.max(20, { message: 'Must be 20 characters or less' }),
+	creatorId: z.string().min(21),
+	walletAddress: z.string().refine((x) => {
+		return validator.isEthereumAddress(x);
+	}),
 	amount: z.string().refine(
 		(x) => {
 			return validator.isInt(x, { gt: 0, lt: 10000 });
 		},
 		{ message: 'Must be between  $1 and $9999' }
 	),
-	address: z.string().refine(validator.isEthereumAddress),
-	expired: z.boolean().optional().default(false),
-	created_at: z.string().optional().default(new Date().toISOString()),
-	creatorId: z.string().min(21)
+	fundedAmount: z.string().refine((x) => {
+		return validator.isInt(x, { min: 0 });
+	}),
+	createdAt: z
+		.string()
+		.optional()
+		.default(new Date().toISOString())
+		.refine((x) => {
+			return validator.isDate(x);
+		}),
+	callStart: z
+		.string()
+		.optional()
+		.refine((x) => {
+			return !x || validator.isDate(x);
+		}),
+	callEnd: z
+		.string()
+		.optional()
+		.refine((x) => {
+			return !x || validator.isDate(x);
+		}),
+	callId: z.string().min(21),
+	status: z.nativeEnum(LinkStatus).default(LinkStatus.ACTIVE)
 });
 
 export type LinkType = z.infer<typeof linkSchema>;
