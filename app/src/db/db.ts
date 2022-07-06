93ced08c4d719dca74b5510b824839479453e5f2
Crypto Joe <crypto.joe@sovereign.science>
2022-07-06 18:36:08 +0800

    cleaned up some default methods

--- app/src/db/db.ts
+++ app/src/db/db.ts
@@ -1,7 +1,7 @@
 import find from 'pouchdb-find';
 import PouchDB from 'pouchdb-node';
 
-const REMOTE_DB_URL = import.meta.env.VITE_REMOTE_COUCHDB_URL || '';
+const REMOTE_DB_URL = import.meta.env.VITE_REMOTE_COUCHDB_URL;
 const LOCAL_DB_URL = import.meta.env.VITE_LOCAL_COUCHDB_URL || 'pouchdb/pcall';
 
 let db: PouchDB.Database;
@@ -11,22 +11,25 @@ export const getDb = (): PouchDB.Database => {
 		try {
 			PouchDB.plugin(find);
 			const localDB = new PouchDB(LOCAL_DB_URL);
-			const remoteDB = new PouchDB(REMOTE_DB_URL);
-
 			db = localDB;
-			localDB
-				.sync(remoteDB, {
-					live: true,
-					retry: true
-				})
-				.then((result) => console.log('sync result: ', result));
 
-			// db.changes({
-			// 	since: 'now',
-			// 	live: true
-			// }).on('change', function (change) {
-			// 	console.log(change);
-			// });
+			if (REMOTE_DB_URL) {
+				const remoteDB = new PouchDB(REMOTE_DB_URL);
+
+				localDB
+					.sync(remoteDB, {
+						live: true,
+						retry: true
+					})
+					.then((result) => console.log('sync result: ', result));
+
+				// db.changes({
+				// 	since: 'now',
+				// 	live: true
+				// }).on('change', function (change) {
+				// 	console.log(change);
+				// });
+			}
 		} catch (err) {
 			console.log(err);
 		}
--- app/src/db/models/agent.ts
+++ app/src/db/models/agent.ts
@@ -18,10 +18,11 @@ export type AgentType = z.infer<typeof AgentSchema>;
 export class AgentDocument extends DocumentBase implements AgentType {
 	public address: string;
 	public walletAddress?: string;
-	public talents?: TalentDocument[];
+	public talents: TalentDocument[];
 	public static type = 'agent';
 	constructor(address: string) {
 		super(AgentDocument.type);
 		this.address = address;
+		this.talents = [];
 	}
 }
