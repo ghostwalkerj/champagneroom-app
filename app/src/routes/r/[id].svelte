82b4b5869f3021f67710213550d00c27f3c90964
Crypto Joe <crypto.joe@sovereign.science>
2022-07-10 15:54:22 +0800

    Moved link detail into component

--- app/src/routes/r/[id].svelte
+++ app/src/routes/r/[id].svelte
@@ -3,6 +3,7 @@
 	import { page } from '$app/stores';
 	import VideoCall from 'components/VideoCall.svelte';
 	import VideoPreview from 'components/VideoPreview.svelte';
+	import LinkDetail from 'components/web3/LinkDetail.svelte';
 	import { gun } from 'db';
 	import { LinkById, type Link } from 'db/models/link';
 	import { userStream, type UserStreamType } from 'lib/userStream';
@@ -64,85 +65,39 @@
 		{#if link}
 			{#if !inCall}
 				<!-- Page header -->
+				<div class="text-center">
+					<h1 class="font-bold text-center text-5xl">Make your pCall</h1>
+					<p class="py-6">Scis vis facere illud pCall. Carpe florem et fac quod nunc vocant.</p>
+				</div>
 				<div
 					class="mx-auto max-w-3xl  sm:px-6 md:flex md:space-x-5 md:items-center md:justify-between lg:max-w-7xl lg:px-8"
 				>
-					<div class="flex flex-col space-x-5 w-full p-2 items-center">
-						<h1 class="font-bold text-center text-5xl">Make your pCall</h1>
-						<p class="py-6">Scis vis facere illud pCall. Carpe florem et fac quod nunc vocant.</p>
-						<div class="flex flex-col w-full gap-6 md:flex-row">
-							<div class="flex flex-col w-full ">
-								<div
-									class="rounded-box h-full bg-base-200 flex-shrink-0 shadow-xl mx-2 grid  p-4 py-8 gap-4 col-span-3 row-span-3 place-items-center xl:mx-0 xl:w-full"
-								>
-									<div class="text-center">
-										<div class="font-extrabold text-lg">This pCall is For</div>
-										<div class="font-extrabold text-3xl">{link.name}</div>
-									</div>
-									<div class="rounded-full flex-none h-48 w-48 mask-circle">
-										<Image
-											src={link.profileImageUrl}
-											alt={link.name}
-											height="48"
-											width="48"
-											class="rounded-full flex-none object-cover mask-circle"
-										/>
-									</div>
-									<StarRating {rating} />
+					<div class="rounded-box h-full bg-base-200">
+						<div>
+							<LinkDetail {link} />
+						</div>
+						<div class="btn-group justify-center pb-6">
+							<button class="btn btn-secondary" on:click={call} disabled={callState != 'ready'}
+								>Call {link.name} Now</button
+							>
+						</div>
+					</div>
 
-									<div class="stats stats-vertical stats-shadow lg:stats-horizontal">
-										<div class="stat">
-											<div class="text-primary w-10 stat-figure">
-												<FaMoneyBillWave />
-											</div>
-											<div class="stat-title">Requested</div>
-											<div class="text-primary stat-value">
-												{formatter.format(Number.parseInt(link.amount))}
-											</div>
-										</div>
-
-										<div class="stat">
-											<div class="text-secondary w-10 stat-figure">
-												<FaMoneyBillWave />
-											</div>
-											<div class="stat-title">Funded</div>
-											<div class="text-secondary stat-value">
-												{formatter.format(Number.parseInt(link.fundedAmount || '0'))}
-											</div>
-										</div>
-									</div>
-									<section
-										class="flex flex-col bg-base-100 rounded-2xl flex-shrink-0 text-white text-center p-4 gap-4 items-center justify-center  md:gap-8 "
-									>
-										<div>Funding Address</div>
-										<div class="break-all">{link.walletAddress}</div>
-									</section>
-
-									<div class="btn-group">
-										<button
-											class="btn btn-secondary"
-											on:click={call}
-											disabled={callState != 'ready'}>Call {link.name} Now</button
-										>
-									</div>
-								</div>
+					<div class="bg-base-200 text-white card">
+						<div class="text-center card-body items-center">
+							<div class="text-2xl card-title">Your Video Preview</div>
+							<div class="container rounded-2xl max-w-2xl">
+								<VideoPreview {us} />
 							</div>
-
-							<div class="bg-base-200 text-white card">
-								<div class="text-center card-body items-center">
-									<div class="text-2xl card-title">Your Video Preview</div>
-									<div class="container rounded-2xl max-w-2xl">
-										<VideoPreview {us} />
-									</div>
-									Call State: {callState}
-								</div>
-							</div>
+							Call State: {callState}
 						</div>
 					</div>
 				</div>
 			{:else}
 				<VideoCall {vc} {us} />
 			{/if}
-		{:else}Searching for your pCall{/if}
+		{:else}
+			<h1>Searching for your pCall</h1>
+		{/if}
 	</main>
 </div>
