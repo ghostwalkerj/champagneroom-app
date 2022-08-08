61a1fecf14d75e8ea48d86229cbb343244e078e4
Crypto Joe <crypto.joe@sovereign.science>
2022-08-08 17:55:57 +0800

    Before adding charts

--- app/src/lib/components/TopTalent.svelte
+++ app/src/lib/components/TopTalent.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
 	import type { AgentDocument } from '$lib/ORM/models/agent';
-
 	export let agent: AgentDocument;
+	const month = new Date().toLocaleString('default', { month: 'long' });
 </script>
 
 <div class="bg-primary text-primary-content w-full card">
 	<div class="text-center card-body items-center">
-		<h2 class="text-2xl card-title">Top Talent</h2>
+		<h2 class="text-2xl card-title">Top Talent - {month}</h2>
 
 		<div class="text-white text-left whitespace-nowrap" />
 	</div>
