722a0c34a892531e71346b6ff339ad6f048d0ad0
Crypto Joe <crypto.joe@sovereign.science>
2022-11-24 12:28:22 +0800

    Wait to render first page until detect mobile

--- app/src/routes/t/[key]/+layout.svelte
+++ app/src/routes/t/[key]/+layout.svelte
@@ -15,27 +15,29 @@
 			browserType.set(result.type);
 		}
 
-		if ($browserType == 'mobile') {
+		if ($browserType == 'mobile' || $browserType == 'tablet') {
 			const mobileUrl = urlJoin($page.url.origin, PUBLIC_MOBILE_PATH, $page.url.pathname);
 			goto(mobileUrl);
 		}
 	}
 </script>
 
-{#if $browserType != 'mobile'}
-	<div class="bg-base-100 navbar">
-		<div class="flex-1">
-			<!-- svelte-ignore a11y-missing-attribute -->
-			<a class="text-xl btn btn-ghost normal-case">
-				<img src="/logo.png" alt="Logo" width="48" /></a
-			>
+{#if browser}
+	{#if $browserType != 'mobile' && $browserType != 'tablet'}
+		<div class="bg-base-100 navbar">
+			<div class="flex-1">
+				<!-- svelte-ignore a11y-missing-attribute -->
+				<a class="text-xl btn btn-ghost normal-case">
+					<img src="/logo.png" alt="Logo" width="48" /></a
+				>
+			</div>
+			<div class="flex-none">
+				<ConnectButton />
+			</div>
 		</div>
-		<div class="flex-none">
-			<ConnectButton />
-		</div>
-	</div>
 
-	<div class="h-full">
-		<slot />
-	</div>
+		<div class="h-full">
+			<slot />
+		</div>
+	{/if}
 {/if}
