98120270edae52c0186faa9cd6dece5221c62784
Crypto Joe <crypto.joe@sovereign.science>
2022-11-27 11:59:16 +0800

    added mobile swift app to mono rep

---
+++ mobile/android/app/src/main/java/app/pcall/pmobile/MainActivity.java
@@ -0,0 +1,5 @@
+package app.pcall.pmobile;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ mobile/tailwind.config.cjs
@@ -0,0 +1,8 @@
+const konstaConfig = require('konsta/config');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = konstaConfig({
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  darkMode: 'class',
+
+});
