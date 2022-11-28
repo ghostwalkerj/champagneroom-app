6e8edad32b3a5a0863bd64077498fb9f3b86a9d8
Crypto Joe <crypto.joe@sovereign.science>
2022-11-28 11:38:10 +0800

    cleanup

---
+++ mobile/src/lib/util.ts
@@ -0,0 +1,6 @@
+import urlJoin from 'url-join';
+
+export const getTalentDB = async (key: string) => {
+  const WEB_URL = import.meta.env.VITE_WEB_URL;
+  const AUTH_PATH = import.meta.env.VITE_AUTH_PATH;
+};
