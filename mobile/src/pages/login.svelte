1281f7ba3d389315f5211018ab4843a5820907e9
Crypto Joe <crypto.joe@sovereign.science>
2022-11-28 09:53:49 +0800

    Before merging

---
+++ mobile/src/pages/login.svelte
@@ -0,0 +1,42 @@
+<script type="ts">
+  import {
+    f7,
+    Page,
+    LoginScreenTitle,
+    List,
+    ListInput,
+    ListButton,
+    BlockFooter,
+  } from 'framework7-svelte';
+  import { setContext } from 'svelte';
+  
+
+  export let f7router;
+
+  let key = '';
+
+  const signIn = () => {
+    
+    });
+  };
+</script>
+
+<Page noToolbar noNavbar noSwipeback loginScreen>
+  <LoginScreenTitle>pCall Creator</LoginScreenTitle>
+  <List form>
+    <ListInput
+      label="Talent Key"
+      type="text"
+      placeholder="Key"
+      value={key}
+      onInput={e => (key = e.target.value)}
+    />
+  </List>
+  <List>
+    <ListButton onClick={signIn}>Sign In</ListButton>
+  </List>
+  <BlockFooter
+    >Some text about login information.<br />Lorem ipsum dolor sit amet,
+    consectetur adipiscing elit.</BlockFooter
+  >
+</Page>
