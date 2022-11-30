71148430d99101d03ed33e0bd7c2d994f6ded64e
Crypto Joe <crypto.joe@sovereign.science>
2022-11-30 15:13:38 +0800

    adding linkMachine

--- mobile/src/pages/home.svelte
+++ mobile/src/pages/home.svelte
@@ -9,12 +9,39 @@
     Navbar,
     Page,
     Row,
+    Card,
+    CardContent,
   } from 'framework7-svelte';
   import { talent, talentDB } from '../lib/stores';
   import type { LinkDocument } from '$lib/ORM/models/link';
+  import { currencyFormatter } from '$lib/util/constants';
+  import {
+    createLinkMachineService,
+    type LinkMachineServiceType,
+  } from '$lib/machines/linkMachine';
+  import type { Subscription } from 'xstate';
 
   $: name = '';
   $: currentLink = null as LinkDocument | null;
+  $: waiting4StateChange = false;
+
+  let linkMachineState =
+    currentLink &&
+    createLinkMachineService(currentLink.linkState).getSnapshot();
+  let linkService: LinkMachineServiceType;
+  let linkSub: Subscription;
+
+  $: canCancelLink =
+    linkMachineState &&
+    linkMachineState.can({
+      type: 'REQUEST CANCELLATION',
+      cancel: undefined,
+    });
+
+  $: canCreateLink =
+    !currentLink ||
+    (linkMachineState && linkMachineState.done) ||
+    (linkMachineState && linkMachineState.matches('inEscrow'));
 
   $: talent.subscribe(talent => {
     if (talent) {
@@ -25,25 +52,71 @@
             .findOne(linkId)
             .exec()
             .then(link => {
-              currentLink = link;
+              if (link) useLink(link);
             });
         }
       });
     }
   });
+
+  const useLinkState = (
+    link: LinkDocument,
+    linkState: LinkDocument['linkState']
+  ) => {
+    if (linkService) linkService.stop();
+    if (linkSub) linkSub.unsubscribe();
+
+    linkService = createLinkMachineService(
+      linkState,
+      link.updateLinkStateCallBack()
+    );
+    linkSub = linkService.subscribe(state => {
+      linkMachineState = state;
+
+      if (state.changed) {
+        canCancelLink = state.can({
+          type: 'REQUEST CANCELLATION',
+          cancel: undefined,
+        });
+        canCreateLink = state.done ?? true;
+      }
+    });
+  };
+
+  const useLink = (link: LinkDocument) => {
+    currentLink = link;
+    waiting4StateChange = false; // link changed, so can submit again
+    useLinkState(link, link.linkState);
+    link.get$('linkState').subscribe(_linkState => {
+      waiting4StateChange = false; // link changed, so can submit again
+      useLinkState(link, _linkState);
+    });
+  };
 </script>
 
 <Page name="home">
   <!-- Top Navbar -->
   <Navbar title="pCall Creator" subtitle={name} />
 
-  <!-- Page content -->
-  <Block strong>
-    <p>
-      Each tab/view may have different layout, different navbar type (dynamic,
-      fixed or static) or without navbar like this tab.
-    </p>
-  </Block>
+  <!-- Current Link -->
+  {#if currentLink}
+    <Card title="Your Outstanding pCall Link">
+      <CardContent>
+        <Row
+          ><Col
+            >Amount Requested: {currencyFormatter.format(
+              currentLink.requestedAmount
+            )}</Col
+          >
+          <Col
+            >Total Funded: {currencyFormatter.format(
+              currentLink.linkState.totalFunding || 0
+            )}</Col
+          >
+        </Row>
+      </CardContent>
+    </Card>
+  {/if}
 
   <BlockTitle>Navigation</BlockTitle>
   <List>
