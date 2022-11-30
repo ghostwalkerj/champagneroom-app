3e3fa6713d7c3021fea3ca914498c4d4c85999b6
Crypto Joe <crypto.joe@sovereign.science>
2022-11-30 13:08:59 +0800

    adding back ts

--- mobile/src/lib/constants.ts
+++ mobile/src/lib/constants.ts
@@ -1,2 +1,4 @@
 export const PUBLIC_RXDB_PASSWORD = import.meta.env.VITE_RXDB_PASSWORD;
 export const PUBLIC_CREATORS_ENDPOINT = import.meta.env.VITE_CREATORS_ENDPOINT;
+export const PUBLIC_GRACE_PERIOD = import.meta.env.VITE_GRACE_PERIOD;
+export const PUBLIC_ESCROW_PERIOD = import.meta.env.VITE_ESCROW_PERIOD;
