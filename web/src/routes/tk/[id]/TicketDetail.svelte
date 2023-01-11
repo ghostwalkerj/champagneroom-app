1ab8f26a9a3e0aa1d0dcf8f832043ec1bec0d4f8
Crypto Joe <crypto.joe@sovereign.science>
2023-01-11 17:20:08 +0800

    Formatting stuff

--- web/src/routes/tk/[id]/TicketDetail.svelte
+++ web/src/routes/tk/[id]/TicketDetail.svelte
@@ -9,121 +9,46 @@
 
   $: profileImage = getProfileImage(ticket.ticketState.reservation.name);
 
-  $: waterMark = true;
-  $: waterMarkText = '';
   $: canBuyTicket = show.showState.status === ShowStatus.BOX_OFFICE_OPEN;
-
-  $: switch (show.showState.status) {
-    case ShowStatus.CANCELLED:
-      waterMarkText = 'Cancelled';
-      break;
-    case ShowStatus.STARTED:
-      waterMarkText = 'Show Started';
-      break;
-    case ShowStatus.FINALIZED:
-    case ShowStatus.ENDED:
-    case ShowStatus.IN_ESCROW:
-    case ShowStatus.IN_DISPUTE:
-      waterMarkText = 'ENDED';
-      break;
-
-    default:
-      waterMark = false;
-  }
 </script>
 
 {#if show}
   <div class="flex justify-center">
     <div
-      class="flex flex-col w-full p-4 max-w-2xl  gap-4 rounded-xl bg-base-200  overflow-auto"
+      class="flex flex-col w-full p-4 max-w-2xl gap-4 rounded-xl bg-base-200  overflow-auto"
     >
-      <div class="flex flex-row justify-between">
-        <div>
-          <div
-            class="grid grid-rows-1 gap-4 grid-flow-col justify-center items-center"
-          >
+      <div class="grid grid-flow-row gap-4">
+        <div class="flex gap-4 ">
+          <div class="">
             <div
-              class="bg-cover  bg-no-repeat rounded-full h-48 w-48 row-span-2"
+              class="bg-cover bg-no-repeat rounded-full h-24 w-24 row-span-2"
               style="background-image: url('{profileImage}')"
             />
-            <form method="post" action="?/buy_ticket">
-              <div class="max-w-xs w-full py-2 form-control ">
-                <!-- svelte-ignore a11y-label-has-associated-control -->
-                <label for="caller" class="label">
-                  <span class="label-text">Your Name</span></label
-                >
-              </div>
-              <div class="max-w-xs w-full py-2 form-control ">
-                <!-- svelte-ignore a11y-label-has-associated-control -->
-                <label for="pin" class="label">
-                  <span class="label-text">8 Digit Pin</span></label
-                >
-                <div class="rounded-md shadow-sm mt-1 relative">
-                  <input
-                    name="pin"
-                    type="text"
-                    class="max-w-xs w-full py-2 pl-6 input input-bordered input-primary"
-                    minlength="8"
-                    maxlength="8"
-                  />
-                </div>
-              </div>
+            <div>
+              {ticket.ticketState.reservation.name}
+            </div>
+          </div>
+          <div class="flex flex-col">
+            <div>
+              {show.talentInfo.name}
+            </div>
+            <div>{show.name}</div>
 
-              <div class="py-4 text-center">
-                <button
-                  class="btn btn-secondary "
-                  type="submit"
-                  disabled={!canBuyTicket}>Reserve</button
-                >
-              </div>
-            </form>
+            <div class="">
+              {durationFormatter(show.duration)}
+            </div>
+            <div class="">
+              {currencyFormatter.format(show.price)}
+            </div>
           </div>
-        </div>
-        <div class="flex justify-center flex-col">
           <div
-            class="relative bg-cover bg-no-repeat bg-center rounded-xl h-48"
+            class="relative bg-cover bg-no-repeat bg-center rounded-xl h-32 w-48"
             style="background-image: url('{show.talentInfo.profileImageUrl}')"
-          >
-            <div class="absolute top-1 left-2 text-lg">{show.name}</div>
-            {#if waterMark}
-              <div
-                class="absolute top-0 left-0 w-full h-full bg-black bg-opacity-50  rounded-xl"
-              />
-              <div
-                class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-7xl lg:text-8xl text-red-900 -rotate-45"
-              >
-                {waterMarkText}
-              </div>
-            {/if}
-          </div>
-          <div>
-            {show.talentInfo.name}
-          </div>
-          <div class="place-self-center">
-            <StarRating rating={show.talentInfo.stats.ratingAvg} />
-          </div>
-          <div
-            class="stats stats-vertical stats-shadow text-center lg:stats-horizontal"
-          >
-            <div class="stat">
-              <div class="stat-title">Duration</div>
-              <div class="text-primary stat-value">
-                {durationFormatter(show.duration)}
-              </div>
-            </div>
-            <div class="stat">
-              <div class="stat-title">Price</div>
-              <div class="text-primary stat-value">
-                {currencyFormatter.format(show.price)}
-              </div>
-            </div>
-            <div class="stat">
-              <div class="stat-title">Tickets Available</div>
-              <div class="text-primary stat-value">
-                {show.showState.ticketsAvailable}
-              </div>
-            </div>
-          </div>
+          />
+        </div>
+        <div class="w-full flex">
+          <div>Payment Address:</div>
+          <div class="btn">Pay</div>
         </div>
       </div>
     </div>
