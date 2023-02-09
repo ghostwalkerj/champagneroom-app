a7f30be90b5deb9a76b5252efa702c0aea31bfb3
Crypto Joe <crypto.joe@sovereign.science>
2023-02-09 17:49:29 +0800

    more pretty

--- web/src/routes/t/[key]/+page.svelte
+++ web/src/routes/t/[key]/+page.svelte
@@ -242,101 +242,104 @@
         <div class="bg-primary text-primary-content card">
           <div class="text-center card-body items-center p-3">
             <h2 class="text-2xl card-title">Create a New Show</h2>
-            <div
-              class="flex flex-col text-white p-2 justify-center items-center"
-            >
+            <div class="flex flex-col w-full">
               <form method="post" action="?/create_show" use:enhance={onSubmit}>
-                <div class="max-w-xs  py-2 form-control">
-                  <!-- svelte-ignore a11y-label-has-associated-control -->
-                  <label class="label">
-                    <span class="label-text">Title</span>
-                  </label>
-                  <input
-                    type="text"
-                    name="name"
-                    class="max-w-xs  py-2 input input-bordered input-primary"
-                    bind:value={showName}
-                    minlength="3"
-                    maxlength="50"
-                  />
-                </div>
-                {#if form?.badName}
-                  <div class="shadow-lg alert alert-error">
-                    Show Name should be between 3 and 50 characters
-                  </div>
-                {/if}
-
-                <div class="max-w-xs w-full py-2 form-control ">
-                  <!-- svelte-ignore a11y-label-has-associated-control -->
-                  <label for="price" class="label">
-                    <span class="label-text">Ticket Price in USD</span></label
-                  >
-                  <div class="rounded-md shadow-sm mt-1 relative">
-                    <div
-                      class="flex pl-3 inset-y-0 left-0 absolute items-center pointer-events-none"
-                    >
-                      <span class="text-gray-500 sm:text-sm"> $ </span>
-                    </div>
+                <div
+                  class="flex flex-col md:flex-row  text-white p-2 justify-center items-center gap-4"
+                >
+                  <div class="form-control flex-grow">
+                    <!-- svelte-ignore a11y-label-has-associated-control -->
+                    <label class="label">
+                      <span class="label-text">Title</span>
+                    </label>
                     <input
                       type="text"
-                      name="price"
-                      class=" max-w-xs w-full py-2 pl-6 input input-bordered input-primary "
-                      placeholder="0.00"
-                      aria-describedby="price-currency"
-                      value={form?.price ?? ''}
+                      name="name"
+                      class="input input-bordered input-primary"
+                      bind:value={showName}
+                      minlength="3"
+                      maxlength="50"
                     />
-                    <div
-                      class="flex pr-3 inset-y-0 right-0 absolute items-center pointer-events-none"
-                    >
-                      <span
-                        class="text-gray-500 sm:text-sm"
-                        id="price-currency"
-                      >
-                        USDC
-                      </span>
-                    </div>
                   </div>
-                  {#if form?.missingPrice}<div
-                      class="shadow-lg alert alert-error"
+                  {#if form?.badName}
+                    <div class="shadow-lg alert alert-error">
+                      Show Name should be between 3 and 50 characters
+                    </div>
+                  {/if}
+
+                  <div class="form-control md:w-40">
+                    <!-- svelte-ignore a11y-label-has-associated-control -->
+                    <label for="price" class="label">
+                      <span class="label-text">Ticket Price in USD</span></label
                     >
-                      Price is required
-                    </div>{/if}
-                  {#if form?.invalidPrice}<div
-                      class="shadow-lg alert alert-error"
-                    >
-                      Invalid Price
-                    </div>{/if}
-                </div>
-                <input type="hidden" name="maxNumTickets" value="1" />
-                <div class="max-w-xs  py-2 form-control">
-                  <!-- svelte-ignore a11y-label-has-associated-control -->
-                  <label class="label">
-                    <span class="label-text"
-                      >Duration ({durationFormatter(showDuration)})</span
-                    >
-                  </label>
-                  <input
-                    type="range"
-                    min="15"
-                    max="120"
-                    bind:value={showDuration}
-                    class="range"
-                    step="15"
-                    name="duration"
-                  />
-                  <div class="w-full flex justify-between text-xs px-2">
-                    <span>|</span>
-                    <span>|</span>
-                    <span>|</span>
-                    <span>|</span>
-                    <span>|</span>
-                    <span>|</span>
-                    <span>|</span>
-                    <span>|</span>
+                    <div class="rounded-md shadow-sm relative">
+                      <div
+                        class="flex pl-3 inset-y-0 left-0 absolute items-center pointer-events-none"
+                      >
+                        <span class="text-gray-500 sm:text-sm"> $ </span>
+                      </div>
+                      <input
+                        type="text"
+                        name="price"
+                        class="w-full py-2 pl-6 input input-bordered input-primary "
+                        placeholder="0.00"
+                        aria-describedby="price-currency"
+                        value={form?.price ?? ''}
+                      />
+                      <div
+                        class="flex pr-3 inset-y-0 right-0 absolute items-center pointer-events-none"
+                      >
+                        <span
+                          class="text-gray-500 sm:text-sm"
+                          id="price-currency"
+                        >
+                          USDC
+                        </span>
+                      </div>
+                    </div>
+                    {#if form?.missingPrice}<div
+                        class="shadow-lg alert alert-error"
+                      >
+                        Price is required
+                      </div>{/if}
+                    {#if form?.invalidPrice}<div
+                        class="shadow-lg alert alert-error"
+                      >
+                        Invalid Price
+                      </div>{/if}
+                  </div>
+
+                  <input type="hidden" name="maxNumTickets" value="1" />
+                  <div class="form-control md:w-1/5">
+                    <!-- svelte-ignore a11y-label-has-associated-control -->
+                    <label class="label">
+                      <span class="label-text"
+                        >Duration ({durationFormatter(showDuration)})</span
+                      >
+                    </label>
+                    <input
+                      type="range"
+                      min="15"
+                      max="120"
+                      bind:value={showDuration}
+                      class="range"
+                      step="15"
+                      name="duration"
+                    />
+                    <div class="w-full flex justify-between text-xs px-2">
+                      <span>|</span>
+                      <span>|</span>
+                      <span>|</span>
+                      <span>|</span>
+                      <span>|</span>
+                      <span>|</span>
+                      <span>|</span>
+                      <span>|</span>
+                    </div>
                   </div>
                 </div>
 
-                <div class="py-4">
+                <div class="py-2">
                   <button
                     class="btn btn-secondary"
                     type="submit"
@@ -395,7 +398,7 @@
         <div class="lg:col-start-3 lg:col-span-1">
           <div class="bg-primary text-primary-content card">
             <div class="text-center card-body items-center  p-3">
-              <h2 class="text-2xl card-title">{talent.name}</h2>
+              <h2 class="text-xl card-title">{talent.name}</h2>
               <div>
                 <ProfilePhoto
                   profileImage={talent.profileImageUrl ||
