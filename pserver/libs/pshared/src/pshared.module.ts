5e447c44bbfa9785a223b814144f6e05600fe4a5
Crypto Joe <crypto.joe@sovereign.science>
2023-05-06 07:35:19 +1000

    reorg

--- pserver/libs/pshared/src/pshared.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PsharedService } from './pshared.service';
-
-@Module({
-  providers: [PsharedService],
-  exports: [PsharedService],
-})
-export class PsharedModule {}
--- pserver/src/show-manager/show-manager.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ShowManagerService } from './show-manager.service';
-
-@Module({
-  providers: [ShowManagerService],
-})
-export class ShowManagerModule {}
