bafb4e7476223ce93ecceec5179ce92558fee0f7
Crypto Joe <crypto.joe@sovereign.science>
2023-05-28 16:09:10 +1000

    Show worker listening to queue

--- app/server.js
+++ app/server.js
@@ -1,31 +1,30 @@
 import { createBullBoard } from '@bull-board/api';
-import { BullMQAdapter } from '@bull-board/api/bullMQAdapter.js';
+import { BullMQAdapter } from '@bull-board/api/bullMQAdapter';
 import { ExpressAdapter } from '@bull-board/express';
 import cors from 'cors';
 import express from 'express';
-import { handler } from './build/handler.js';
-import { showQueue } from './dist/workers/showWorker.js';
-
+import { handler } from './build/handler';
+import { queue, worker } from './dist/mq/showQueue';
+const app = express();
 const corsOptions = {
   origin: '*',
   optionsSuccessStatus: 200,
 };
 
+// Workers
+worker.run();
+
+// Bull Dashboard
 const serverAdapter = new ExpressAdapter();
 serverAdapter.setBasePath('/admin/queues');
-
 const { addQueue, removeQueue, setQueues, replaceQueues } = createBullBoard({
-  queues: [new BullMQAdapter(showQueue)],
+  queues: [new BullMQAdapter(queue)],
   serverAdapter: serverAdapter,
 });
-
-const app = express();
 app.use('/admin/queues', serverAdapter.getRouter());
 
-// let SvelteKit handle everything else, including serving prerendered pages and static assets
-
+// Svelte App
 app.use(cors(corsOptions), handler);
-
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log('pCall server running on: ', port);
--- app/src/lib/workers/showWorker.ts
+++
@@ -1,5 +0,0 @@
-import { EntityType } from '$lib/util/constants.js';
-import { REDIS_OPTIONS } from '$lib/util/secrets.js';
-import { Queue } from 'bullmq';
-
-export const showQueue = new Queue(EntityType.SHOW, REDIS_OPTIONS);
