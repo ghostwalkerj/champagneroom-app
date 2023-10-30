0f981256843573ab12fa0d21f79a9d6d8912aeb0
Crypto Joe <crypto.joe@sovereign.science>
2023-10-30 18:31:11 +1000

    before adding matching lib

--- app/src/routes/app/creator/[[secret]]/+page.server.ts
+++ app/src/routes/app/creator/[[secret]]/+page.server.ts
@@ -4,15 +4,14 @@ import { Queue } from 'bullmq';
 import type IORedis from 'ioredis';
 
 import {
-    BITCART_API_URL,
-    BITCART_EMAIL,
-    BITCART_PASSWORD
+  BITCART_API_URL,
+  BITCART_EMAIL,
+  BITCART_PASSWORD
 } from '$env/static/private';
 
 import type { CancelType } from '$lib/models/common';
 import { CancelReason, CurrencyType } from '$lib/models/common';
 import type { CreatorDocument } from '$lib/models/creator';
-import { Creator } from '$lib/models/creator';
 import { Show, ShowStatus } from '$lib/models/show';
 import { Wallet, WalletStatus } from '$lib/models/wallet';
 
@@ -24,40 +23,34 @@ import type { ShowQueueType } from '$lib/workers/showWorker';
 
 import { ActorType, EntityType } from '$lib/constants';
 import { rateCryptosRateGet } from '$lib/ext/bitcart';
-import { PayoutJobType, PayoutReason, createAuthToken } from '$lib/payment';
+import { createAuthToken, PayoutJobType, PayoutReason } from '$lib/payment';
 import { getShowMachineServiceFromId } from '$lib/server/machinesUtil';
 
-import type { Actions, PageServerLoad, RequestEvent } from './[address]/$types';
+import type { Actions, PageServerLoad, RequestEvent } from './$types';
 
 export const actions: Actions = {
-  update_profile_image: async ({ params, request }: RequestEvent) => {
-    const address = params.address;
-    if (address === null) {
-      throw error(404, 'Address not found');
-    }
+  update_profile_image: async ({ locals, request }: RequestEvent) => {
     const data = await request.formData();
     const url = data.get('url') as string;
     if (!url) {
       return fail(400, { url, missingUrl: true });
     }
-    const creator = await Creator.findOneAndUpdate(
-      { 'user.address': address },
-      { profileImageUrl: url }
-    ).exec();
+    const creator = locals.creator as CreatorDocument;
+    creator.profileImageUrl = url;
+    await creator.save();
 
     return {
       success: true,
       creator: creator?.toObject({ flattenObjectIds: true, flattenMaps: true })
     };
   },
-  create_show: async ({ params, request }) => {
+  create_show: async ({ locals, request }) => {
     const data = await request.formData();
     const price = data.get('price') as string;
     const name = data.get('name') as string;
     const duration = data.get('duration') as string;
     const capacity = data.get('capacity') as string;
     const coverImageUrl = data.get('coverImageUrl') as string;
-    const address = params.address;
 
     if (!name || name.length < 3 || name.length > 50) {
       return fail(400, { name, badName: true });
@@ -69,12 +62,7 @@ export const actions: Actions = {
     if (Number.isNaN(+price) || +price < 1 || +price > 10_000) {
       return fail(400, { price, invalidPrice: true });
     }
-
-    const creator = (await Creator.findOne({ 'user.address': address })
-      .orFail(() => {
-        throw error(404, 'Creator not found');
-      })
-      .exec()) as CreatorDocument;
+    const creator = locals.creator as CreatorDocument;
 
     const show = await Show.create({
       price: {
@@ -108,13 +96,9 @@ export const actions: Actions = {
       show: show.toObject({ flattenObjectIds: true, flattenMaps: true })
     };
   },
-  cancel_show: async ({ request, params, locals }) => {
-    const address = params.address;
+  cancel_show: async ({ request, locals }) => {
     const data = await request.formData();
     const showId = data.get('showId') as string;
-    if (address === null) {
-      throw error(404, 'Address not found');
-    }
 
     if (showId === null) {
       throw error(404, 'Show ID not found');
