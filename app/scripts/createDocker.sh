fcfd977df47398a010f5c826550ba8c23699fbe5
Crypto Joe <crypto.joe@sovereign.science>
2023-11-02 09:56:50 +1000

    cleanup versioning

--- app/scripts/createDocker.sh
+++ app/scripts/createDocker.sh
@@ -4,10 +4,10 @@ DOCKER_DEFAULT_PLATFORM=linux/amd64
 BUILDKIT_PROGRESS=plain
 
 cd ../
-yarn version $VERSION
+yarn version --$VERSION
 docker system prune -af
 yarn build
 yarn package
-docker-compose --env-file ../.env.stage -f ../docker-compose.stage.yml build
+docker-compose --env-file .env.stage -f docker-compose.stage.yml build
 docker tag pcall registry.digitalocean.com/champagne/pcall
 docker push registry.digitalocean.com/champagne/pcall
