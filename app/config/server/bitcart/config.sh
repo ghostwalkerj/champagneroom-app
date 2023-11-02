962ed206778a5655616913868d0e4746d956f797
Crypto Joe <crypto.joe@sovereign.science>
2023-11-03 09:37:03 +1000

    updated bitcart settings

--- app/config/server/bitcart/config.sh
+++ app/config/server/bitcart/config.sh
@@ -4,7 +4,7 @@
 export BITCART_CRYPTOS=eth
 export ETH_NETWORK=sepolia
 export ETH_SERVER=https://sepolia.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161
-
+export BITCART_EXCLUDE_COMPONENTS=store
 
 export BITCART_HOST=host.docker.internal
 export BITCART_REVERSEPROXY="nginx"
