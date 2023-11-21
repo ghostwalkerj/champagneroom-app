54d39df2d91c3a83c1b3b2f201548f4e3dfa8778
Crypto Joe <crypto.joe@sovereign.science>
2023-11-21 13:37:13 +1000

    reformatted for mobile

--- app/src/routes/app/ticket/[[id]]/TicketDetail.svelte
+++ app/src/routes/app/ticket/[[id]]/TicketDetail.svelte
@@ -27,23 +27,36 @@
   const currency = ticket.price.currency || 'USD';
   const customerName = user.name;
   const ticketPrice = currencyFormatter(currency).format(ticket.price.amount);
+
+  let isFullScreen = false;
+
+  function toggleFullScreen() {
+    isFullScreen = !isFullScreen;
+  }
 </script>
 
 <div class="flex justify-center font-CaviarDreams">
   <div
-    class="flex flex-col w-full max-w-2xl rounded-xl bg-black overflow-auto h-32 lg:h-auto"
+    class="flex flex-col md:w-full max-w-4xl rounded-xl bg-black overflow-hidden md:h-auto"
   >
-    <div class="group grid grid-cols-1 lg:grid-cols-4 h-full">
-      <!-- Image Section -->
-      <div class="relative lg:col-span-1">
+    <!-- Adjust grid layout: 2 columns on mobile, 5 on medium and larger screens -->
+    <div class="group grid grid-cols-2 md:grid-cols-5 h-full md:min-h-[200px]">
+      <!-- Image Section: Take full width on mobile, 1/5th on medium and larger screens -->
+      <!-- svelte-ignore a11y-click-events-have-key-events -->
+      <div
+        class="col-span-2 md:col-span-1 relative"
+        on:click={toggleFullScreen}
+      >
+        <!-- Add click event to open full-screen image -->
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
         <div
-          class="grid grid-flow-row bg-center bg-cover w-full h-48 lg:h-full opacity-70"
+          class="grid grid-flow-row bg-center bg-cover w-full h-48 md:h-full opacity-70 cursor-pointer"
           style="background-image: url('{showCoverImageUrl}');"
         />
         <div class="absolute inset-0 flex justify-center items-center">
           <div class="w-fit animate-pulse">
             <div
-              class="-rotate-45 origin-bottom-right whitespace-nowrap font-bold text-primary ring-2 ring-primary p-2 ring-inset rounded-xl"
+              class="-rotate-45 whitespace-nowrap font-bold text-primary ring-2 ring-primary p-2 ring-inset rounded-xl"
             >
               {creatorName}
             </div>
@@ -51,49 +64,42 @@
         </div>
       </div>
 
-      <!-- Content Section -->
+      <!-- Content Section: Take full width on mobile, 3/5th on medium and larger screens -->
       <div
-        class="col-span-3 relative flex flex-col p-4 text-info font-bold text-sm h-full place-content-center"
+        class="col-span-2 md:col-span-3 relative flex flex-col p-4 text-info font-bold text-sm h-full place-content-center"
       >
         <!-- Ticket Status Overlays -->
-        {#if ticket.ticketState.status === TicketStatus.CANCELLED}
+        {#if ticket.ticketState.status === TicketStatus.CANCELLED || ticket.ticketState.status === TicketStatus.FINALIZED}
           <div
-            class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-2xl whitespace-nowrap font-extrabold text-primary ring-2 ring-primary bg-base-200 p-2 ring-inset rounded-xl z-20 opacity-70 -rotate-[20deg]"
+            class="absolute top-4 left-1/2 transform -translate-x-1/2 text-xl lg:text-2xl whitespace-normal font-extrabold text-primary ring-2 ring-primary bg-base-200 p-2 ring-inset rounded-xl z-20 opacity-70"
           >
-            Ticket Cancelled
-          </div>
-        {/if}
-
-        {#if ticket.ticketState.status === TicketStatus.FINALIZED}
-          <div
-            class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-2xl whitespace-nowrap font-extrabold text-primary ring-2 ring-primary bg-base-200 p-2 ring-inset rounded-xl z-20 opacity-70 -rotate-[20deg]"
-          >
-            Show's Over
+            {ticket.ticketState.status === TicketStatus.CANCELLED
+              ? 'Ticket Cancelled'
+              : "Show's Over"}
           </div>
         {/if}
 
         <!-- Background Logo -->
         <div
-          class="bg-center bg-cover h-28 opacity-10 absolute inset-0 top-2"
+          class="bg-center bg-cover opacity-10 absolute inset-0 top-2"
           style="background-image: url('{Config.Path
             .staticUrl}/assets/logo-horizontal-tr.png'); z-index: 0;"
         />
 
-        <!-- Ticket Information -->
-        <div>Ticket Reserved for: {customerName}</div>
-        <div class="capitalize">Ticket Status: {ticketStatus}</div>
-        <div class="text-sm">Price: {ticketPrice}{currency}</div>
+        <!-- Ticket Information (Aligned at the bottom) -->
+        <div class="mt-auto">
+          <div>Ticket Reserved for: {customerName}</div>
+          <div class="capitalize">Ticket Status: {ticketStatus}</div>
+          <div class="text-sm">Price: {ticketPrice}{currency}</div>
+        </div>
       </div>
 
-      <!-- Right Side Content -->
-      <div class="relative border-l-2 border-dashed lg:col-span-1">
-        <div
-          class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 -rotate-45 whitespace-nowrap font-extrabold text-primary ring-2 ring-primary p-2 ring-inset rounded-xl animate-pulse opacity-70"
-        >
-          {creatorName}
-        </div>
-
-        <div class="m-auto font-bold absolute inset-0 text-info">
+      <!-- Right Side Content: Take full width on mobile, 1/5th on medium and larger screens -->
+      <div
+        class="col-span-2 md:col-span-1 relative flex items-center border-t-2 md:border-l-2 md:border-t-0 border-dashed pb-4 md:pb-0"
+      >
+        <!-- Added dashed border -->
+        <div class="m-auto font-bold text-info">
           <div class="text-xl p-2"><a href={showUrl}>{showName}</a></div>
           <div class="text-sm">Duration: {showDuration}</div>
           <div class="text-sm">Price: {ticketPrice}{currency}</div>
@@ -101,4 +107,19 @@
       </div>
     </div>
   </div>
+  <!-- Full Screen Image Overlay -->
+  {#if isFullScreen}
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <div
+      class="fixed top-0 left-0 w-screen h-screen flex justify-center items-center bg-black bg-opacity-80 z-50"
+      on:click={toggleFullScreen}
+    >
+      <!-- svelte-ignore a11y-img-redundant-alt -->
+      <img
+        class="max-h-full max-w-full cursor-pointer"
+        src={showCoverImageUrl}
+        alt="Full Screen Image"
+      />
+    </div>
+  {/if}
 </div>
