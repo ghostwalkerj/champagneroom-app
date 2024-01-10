5e639876e2cc614bc881a9bf713e7faa5e287e46
Martin Diaz <midiazgt@gmail.com>
2024-01-09 23:07:00 -0300

    A lot about creators dashboard

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
   import ReserveShowForm from '$components/forms/ReserveShowForm.svelte';
   import CRUDRoomForm from '$components/forms/CRUDRoomForm.svelte';
+  import WithdrawForm from '$components/forms/WithdrawForm.svelte';
+  import TransactionsActivity from '$components/TransactionsActivity.svelte';
+  import EndShowForm from '$components/forms/EndShowForm.svelte';
   import '@fontsource/roboto';
   import '@fontsource/roboto/300.css';
   import '@fontsource/roboto/500.css';
@@ -14,7 +17,11 @@
   const modalRegistry: Record<string, ModalComponent> = {
     // Set a unique modal ID, then pass the component reference
     ReserveShowForm: { ref: ReserveShowForm },
+    WithdrawForm: { ref: WithdrawForm },
+    TransactionsActivity: { ref: TransactionsActivity },
+    EndShowForm: { ref: EndShowForm },
     CRUDRoomForm: { ref: CRUDRoomForm }
+
     // ...
   };
 </script>
