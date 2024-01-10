ffc177d9ee6c2496c5ded5e030b23e97b7cd4bd5
Martin Diaz <midiazgt@gmail.com>
2024-01-09 23:07:00 -0300

    A lot about creators dashboard

--- app/src/components/WalletDetail.svelte
+++ app/src/components/WalletDetail.svelte
@@ -14,7 +14,7 @@
   export let exchangeRate = 0;
   export let form: ActionData;
   export let wallet: WalletDocumentType;
-  export let whitdrawForm: any;
+  export let withdrawForm: any;
 
   $: hasTransactions =
     wallet && wallet.earnings.length + wallet.payouts.length > 0;
@@ -24,74 +24,68 @@
   $: payouts = wallet?.payouts;
   $: availableBalance = wallet?.availableBalance || 0;
 
-
   const modal: ModalSettings = {
     type: 'component',
     component: 'WithdrawForm',
     meta: {
-      form: whitdrawForm,
+      form: withdrawForm,
       wallet,
       exchangeRate,
       destination
     }
   };
 
-const transactionsModal: ModalSettings = {
+  const transactionsModal: ModalSettings = {
     type: 'component',
     component: 'TransactionsActivity',
     meta: {
-      earnings:earnings,
+      earnings: earnings,
       payouts: payouts
     }
   };
 </script>
 
 {#if wallet}
-    <div class="bg-custom p-4 rounded flex flex-col gap-4 justify-center items-center">
-
-      <div class="flex flex-col gap-0 items-center text-center">
-        <h2 class="text-xl font-semibold flex gap-2 items-center">
-          <Icon
-        class="text-secondary"
-        icon="carbon:wallet"
-      />
-          Wallet</h2>
-        <small class="lowercase text-base">Status: {wallet.status}</small>
-      </div>
-
-      <div class="text-center">
-        <p>Balance: 
-          {#if exchangeRate > 0}
-            <span class="font-semibold">
-              {currencyFormatter(CurrencyType.USD, 2).format(
-                availableBalance * exchangeRate
-              )} USD
-            </span>
-          {/if}
-        </p>
-        <p> {currencyFormatter(wallet.currency).format(availableBalance)}</p>
-      </div>
-
-      <div
-            class="flex gap-2"
-          >
-            <button
-              class="btn variant-soft-secondary btn-sm neon-secondary"
-              disabled={!hasTransactions}
-              on:click={() => modalStore.trigger(transactionsModal)}
-            >
-              Transactions
-            </button>
-            <button
-              class="btn variant-soft-secondary btn-sm neon-secondary"
-              disabled={!hasAvailableBalance}
-              on:click={() => modalStore.trigger(modal)}
-            >
-              Withdraw
-            </button>
-          </div>
+  <div
+    class="bg-custom p-4 rounded flex flex-col gap-4 justify-center items-center"
+  >
+    <div class="flex flex-col gap-0 items-center text-center">
+      <h2 class="text-xl font-semibold flex gap-2 items-center">
+        <Icon class="text-secondary" icon="carbon:wallet" />
+        Wallet
+      </h2>
+      <small class="lowercase text-base">Status: {wallet.status}</small>
     </div>
 
+    <div class="text-center">
+      <p>
+        Balance:
+        {#if exchangeRate > 0}
+          <span class="font-semibold">
+            {currencyFormatter(CurrencyType.USD, 2).format(
+              availableBalance * exchangeRate
+            )} USD
+          </span>
+        {/if}
+      </p>
+      <p>{currencyFormatter(wallet.currency).format(availableBalance)}</p>
+    </div>
+
+    <div class="flex gap-2">
+      <button
+        class="btn variant-soft-secondary btn-sm neon-secondary"
+        disabled={!hasTransactions}
+        on:click={() => modalStore.trigger(transactionsModal)}
+      >
+        Transactions
+      </button>
+      <button
+        class="btn variant-soft-secondary btn-sm neon-secondary"
+        disabled={!hasAvailableBalance}
+        on:click={() => modalStore.trigger(modal)}
+      >
+        Withdraw
+      </button>
+    </div>
+  </div>
 {/if}
-
-
