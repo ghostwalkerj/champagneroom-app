f52b2eb9ab717685bcf25afd012a762fde533174
Crypto Joe <crypto.joe@sovereign.science>
2024-01-12 17:44:09 +1000

    Cleaning up room

--- app/src/routes/api/v1/upload/+server.ts
+++ app/src/routes/api/v1/upload/+server.ts
@@ -1,5 +1,7 @@
 import { json } from '@sveltejs/kit';
 
+import { WEB3STORAGE_KEY, WEB3STORAGE_PROOF } from '$env/static/private';
+
 import { web3Upload } from '$lib/server/upload';
 
 import type { RequestHandler } from './$types';
@@ -7,7 +9,7 @@ import type { RequestHandler } from './$types';
 export const POST: RequestHandler = async ({ request }) => {
   const body = await request.formData();
   const file = body.get('file') as File;
-  const response = web3Upload(file);
+  const response = web3Upload(WEB3STORAGE_KEY, WEB3STORAGE_PROOF, file);
   return response
     ? json({ url: response })
     : new Response(undefined, { status: 400 });
