7729e153d10d762c2105496d1463806f0e30a5b6
Crypto Joe <j@soveriegn.science>
2024-04-11 07:40:02 +1000

    Update user creation process, add role-based functionality, and improve error handling.

--- app/src/routes/app/signup/+page.server.ts
+++ app/src/routes/app/signup/+page.server.ts
@@ -1,42 +1,105 @@
 import { fail } from '@sveltejs/kit';
-import * as web3 from 'web3';
+import type { async } from 'rxjs';
 
 import { AUTH_SIGNING_MESSAGE } from '$env/static/private';
 
-import { Agent } from '$lib/models/agent';
+import { Agent, type AgentDocument } from '$lib/models/agent';
+import { Creator } from '$lib/models/creator';
 import { User, type UserDocument } from '$lib/models/user';
 import { Wallet } from '$lib/models/wallet';
 
 import config from '$lib/config';
-import { AuthType, EntityType } from '$lib/constants';
+import { AuthType, EntityType, UserRole } from '$lib/constants';
+import { verifySignature } from '$lib/server/auth';
 
 import type { Actions, PageServerLoad } from './$types';
 
-const verifySignature = (
-  message: string,
-  address: string,
-  signature: string
-) => {
+const createUser = async ({
+  request,
+  role
+}: {
+  request: Request;
+  role: UserRole;
+}) => {
+  const data = await request.formData();
+  const name = data.get('name') as string;
+  const address = data.get('address') as string;
+  const message = data.get('message') as string;
+  const signature = data.get('signature') as string;
+
+  // Validation
+  if (!name || name.length < 3 || name.length > 50) {
+    return fail(400, { name, badName: true });
+  }
+
+  // Verify Auth
+  if (!verifySignature(message, address, signature)) {
+    return fail(400, { invalidSignature: true });
+  }
+
+  // Check if existing user, if so, throw new Error("User already exists");
+  const user = (await User.findOne({
+    address
+  })) as UserDocument;
+  if (user) {
+    return user.isAgent()
+      ? fail(400, { alreadyAgent: true })
+      : fail(400, { alreadyUser: true });
+  }
+
   try {
-    const signerAddr = web3.eth.accounts.recover(message, signature);
-    if (signerAddr.toLowerCase() !== address.toLowerCase()) {
-      return false;
-    }
-    return true;
+    // Create User
+    const wallet = new Wallet();
+    wallet.save();
+
+    const user = await User.create({
+      name,
+      authType: AuthType.SIGNING,
+      address: address.toLowerCase(),
+      wallet: wallet._id,
+      roles: [role]
+    });
+    return {
+      success: true,
+      user
+    };
   } catch (error) {
-    console.error(error);
-    return false;
+    console.error('err', error);
+    return fail(400, { err: JSON.stringify(error) });
   }
 };
 
 export const actions: Actions = {
-  create_agent: async ({ request }) => {
+  create_agent: async ({ request }: { request: Request }) => {
+    try {
+      const result = await createUser({ request, role: UserRole.AGENT });
+      if ('success' in result) {
+        Agent.create({
+          user: result.user._id,
+          defaultCommissionRate: config.UI.defaultCommissionRate
+        });
+
+        return {
+          success: true,
+          returnPath: config.PATH.agent
+        };
+      } else {
+        return result;
+      }
+    } catch (error) {
+      console.error('err', error);
+      return fail(400, { err: JSON.stringify(error) });
+    }
+  },
+  create_creator: async ({ request }: { request: Request }) => {
     const data = await request.formData();
-    const name = (data.get('name') as string).trim();
-    const address = (data.get('address') as string).trim().toLowerCase();
+    const name = data.get('name') as string;
+    const address = data.get('address') as string;
     const message = data.get('message') as string;
     const signature = data.get('signature') as string;
-    const returnPath = config.PATH.agent;
+    const agentId = data.get('agentId') as string;
+
+    const returnPath = config.PATH.creator;
 
     // Validation
     if (!name || name.length < 3 || name.length > 50) {
@@ -48,42 +111,70 @@ export const actions: Actions = {
       return fail(400, { invalidSignature: true });
     }
 
-    // Check if existing user, if so, throw new Error("User already exists");
-    const user = (await User.findOne({
-      address
-    })) as UserDocument;
+    const agent = agentId
+      ? await Agent.findById(agentId)
+      : (undefined as AgentDocument | undefined);
+
+    // Check if existing user, if so, add the role
+    const user = await User.findOne({ address: address.toLowerCase() });
     if (user) {
-      return user.isAgent()
-        ? fail(400, { alreadyAgent: true })
-        : fail(400, { alreadyUser: true });
-    }
+      if (user.roles.includes(UserRole.CREATOR)) {
+        return fail(400, { alreadyCreator: true });
+      } else {
+        user.roles.push(UserRole.CREATOR);
+        user.name = name;
+        user.updateOne(
+          {
+            roles: user.roles,
+            name
+          },
+          { runValidators: true }
+        );
 
-    try {
-      // Create User
-      const wallet = new Wallet();
-      wallet.save();
+        Creator.create({
+          user: user._id,
+          commissionRate: agent?.defaultCommissionRate ?? 0,
+          agent: agent?._id ?? undefined
+        });
+        agent &&
+          User.updateOne(
+            { _id: agent?.user },
+            { $inc: { referralCount: 1 } }
+          ).exec();
+      }
+    } else {
+      try {
+        const wallet = await Wallet.create({});
 
-      const user = await User.create({
-        name,
-        authType: AuthType.SIGNING,
-        address: address.toLowerCase(),
-        wallet: wallet._id,
-        roles: [EntityType.AGENT]
-      });
+        const user = await User.create({
+          name,
+          authType: AuthType.SIGNING,
+          address: address.toLocaleLowerCase(),
+          wallet: wallet._id,
+          payoutAddress: address.toLocaleLowerCase(),
+          roles: [EntityType.CREATOR],
+          profileImageUrl
+        });
 
-      // Create Agent
-      Agent.create({
-        user: user._id,
-        defaultCommissionRate: config.UI.defaultCommissionRate
-      });
+        Creator.create({
+          user: user._id,
+          commissionRate: agent?.defaultCommissionRate ?? 0,
+          agent: agent?._id ?? undefined
+        });
 
+        agent &&
+          User.updateOne(
+            { _id: agent?.user },
+            { $inc: { referralCount: 1 } }
+          ).exec();
+      } catch (error) {
+        console.error('err', error);
+        return fail(400, { err: JSON.stringify(error) });
+      }
       return {
         success: true,
         returnPath
       };
-    } catch (error) {
-      console.error('err', error);
-      return fail(400, { err: JSON.stringify(error) });
     }
   }
 };
