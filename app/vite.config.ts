ebe1a8394cb0514762c2e91299a3d294a282bbeb
Crypto Joe <crypto.joe@sovereign.science>
2024-04-18 12:02:59 +1000

    Update test configuration and setup files for in memDB

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,18 +1,18 @@
-/// <reference types="vitest" />
-
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, type UserConfig } from 'vite';
 import EnvironmentPlugin from 'vite-plugin-environment';
 import mkcert from 'vite-plugin-mkcert';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import type { InlineConfig } from 'vitest';
 
-export default defineConfig({
- test: {
+type ViteConfig = UserConfig & { test: InlineConfig };
+const config: ViteConfig = {
+  // other config
+  test: {
       globals: true,
       include: ['tests/unit/*.{test,spec}.ts'],
       setupFiles: ['./setup/setupTests.ts', './setup/mongoMemoryServer.ts']
     },
-
   mode: 'development',
 
   define: {
@@ -76,4 +76,5 @@ export default defineConfig({
     }
   },
 
-});
+};
+export default defineConfig(config);
