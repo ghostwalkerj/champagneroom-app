64f681451bcd5a80198b0a6a66c423d4e7dc8496
Crypto Joe <crypto.joe@sovereign.science>
2024-04-23 10:25:06 +1000

    Refactor createUser test in user.test.ts

--- app/tests/unit/user.test.ts
+++ app/tests/unit/user.test.ts
@@ -8,28 +8,30 @@ import { Wallet } from '$lib/models/wallet';
 import config from '$lib/config';
 import { AuthType, EntityType } from '$lib/constants';
 
-it('inserts and reads a User', async () => {
-  const password = generateSillyPassword({
-    wordCount: 2
+describe('createUser', () => {
+  it('inserts and reads a User', async () => {
+    const password = generateSillyPassword({
+      wordCount: 2
+    });
+    const secret = nanoid();
+
+    const wallet = new Wallet();
+
+    await wallet.save();
+
+    const user = new User({
+      name: 'test123',
+      authType: AuthType.PATH_PASSWORD,
+      secret,
+      wallet: wallet._id,
+      roles: [EntityType.CREATOR],
+      referralCode: nanoid(10),
+      password,
+      profileImageUrl: config.UI.defaultProfileImage
+    });
+
+    expect(user).toHaveProperty('name');
+    expect(user).toHaveProperty('password');
+    expect(user?.name).toBe('test123');
   });
-  const secret = nanoid();
-
-  const wallet = new Wallet();
-
-  await wallet.save();
-
-  const user = new User({
-    name: 'test123',
-    authType: AuthType.PATH_PASSWORD,
-    secret,
-    wallet: wallet._id,
-    roles: [EntityType.CREATOR],
-    referralCode: nanoid(10),
-    password,
-    profileImageUrl: config.UI.defaultProfileImage
-  });
-
-  expect(user).toHaveProperty('name');
-  expect(user).toHaveProperty('password');
-  expect(user?.name).toBe('test123');
 });
