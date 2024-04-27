f314a0d389e21ba6edc39f09337ab4e86c0d498e
Crypto Joe <j@soveriegn.science>
2024-04-27 12:06:05 +1000

    Update dependencies and fix file upload in Svelte app

--- app/src/lib/server/upload.ts
+++ app/src/lib/server/upload.ts
@@ -1,40 +1,32 @@
-import { CarReader } from '@ipld/car';
-import type { API } from '@ucanto/core';
-import { importDAG } from '@ucanto/core/delegation';
 // eslint-disable-next-line @typescript-eslint/naming-convention
-import * as Signer from '@ucanto/principal/ed25519';
-import { StoreMemory } from '@web3-storage/access/stores/store-memory';
-import { create } from '@web3-storage/w3up-client';
-import type { SharedSpace } from '@web3-storage/w3up-client/space';
+import { ObjectManager } from '@filebase/sdk';
+import urlJoin from 'url-join';
 
 import { env } from '$env/dynamic/private';
 
-async function parseProof(data: string): Promise<any> {
-  const reader = await CarReader.fromBytes(Buffer.from(data, 'base64'));
-  const blocks: Array<API.Transport.Block<any, number, number, 1>> = [];
-  for await (const block of reader.blocks()) {
-    blocks.push(block as API.Transport.Block<any, number, number, 1>);
-  }
-  return importDAG(blocks);
-}
+export const ipfsUpload = async (file: File): Promise<string> => {
+  // Initialize ObjectManager
+  const bucketName = env.FILEBASE_BUCKET_NAME;
+  const thing: ObjectManager = new ObjectManager(bucketName, '1', '2');
+  // const objectManager = new ObjectManager(
+  //   'ipfs',
+  //   'https://ipfs.infura.io:5001',
+  //   {
+  //     bucket: bucketName
+  //   }
+  // );
+  // const objectName = nanoid();
+  // const uploadedObject = await objectManager.upload(
+  //   objectName,
+  //   file.stream(),
+  //   {},
+  //   {
+  //     bucket: bucketName
+  //   }
+  // );
 
-/**
- * Uploads an image to Web3 storage.
- * @param key Key for the principal
- * @param proof Proof for the operation, base64 encoded CAR file
- * @param image Image file to upload
- */
-export const web3Upload = async (
-  key: string,
-  proof: string,
-  image: File
-): Promise<string> => {
-  const principal = Signer.parse(key);
-  const client = await create({ principal, store: new StoreMemory() });
-  const parsedProof = await parseProof(proof);
+  //const url = urlJoin(env.FILEBASE_IPFS_GATEWAY || '', uploadedObject.cid);
+  const url = urlJoin(env.FILEBASE_IPFS_GATEWAY || '', 'cid');
 
-  const space: SharedSpace = await client.addSpace(parsedProof);
-  await client.setCurrentSpace(space.did());
-  const cid = await client.uploadFile(image);
-  return 'https://' + cid.toString() + env.WEB3STORAGE_DOMAIN;
+  return url;
 };
--- app/src/routes/api/v1/upload/+server.ts
+++ app/src/routes/api/v1/upload/+server.ts
@@ -1,18 +1,14 @@
 import { json } from '@sveltejs/kit';
 
-import { env } from '$env/dynamic/private';
-
-import { web3Upload } from '$lib/server/upload';
+import { ipfsUpload } from '$lib/server/upload';
 
 import type { RequestHandler } from './$types';
 
 export const POST: RequestHandler = async ({ request }) => {
   const body = await request.formData();
   const file = body.get('file') as File;
-  const response =
-    env.WEB3STORAGE_KEY && env.WEB3STORAGE_PROOF
-      ? web3Upload(env.WEB3STORAGE_KEY, env.WEB3STORAGE_PROOF, file)
-      : undefined;
+  const response = ipfsUpload(file);
+
   return response
     ? json({ url: response })
     : new Response(undefined, { status: 400 });
